7dd96bad0aa347281dcd3e4abafd9506e0b39dda
tingchaoxi <115476733+tingchaoxi@users.noreply.github.com>
2022-11-20 09:44:01 +0800

    Add files via upload

---
+++ Project11_20/源.cpp
@@ -0,0 +1,80 @@
+
+#include<stdio.h>
+int main()
+{
+	int line = 0;
+	int input = 0;
+	printf("加入不？\n\n\n\n\n");
+	scanf_s("%d", &input);
+	if ("input==不要");
+	{
+		return 0; 
+	}
+	if ("input==要")
+
+	while (line < 100)
+	{
+		printf("桥一行代码\n");
+		line++;
+
+	}
+	while (line <= 1000)
+	{
+		printf("\n\n\n\n\n好好好\n\n\n");
+		line++;
+	}
+	return 0;
+}
+
+
+
+
+
+
+//#include <stdio.h>
+//#include "stdio.h"
+//int main()
+//{
+//	int aa = 0;
+//	printf("哈哈");
+//	scanf("%d", &aa);
+//	return 0;
+//}
+
+
+
+
+
+
+//#include <stdio.h>
+//#include "stdio.h"
+//int main()
+//{
+//	int inpu = 0;
+//	printf("要不要？（1/0/3）"); 
+//	scanf("%d", &inpu);
+//	if (inpu == 1)
+//	{
+//		printf("要");
+//	}
+//	/* if (input == 0);
+//	 {printf("不要"); }*/
+//	 else 
+//	 {
+//		 printf("请输入1或0");
+//	 }
+//	return 0;
+//}
+
+
+
+
+//#include <string.h>
+//#include <stdio.h>
+//int main()
+//{
+//    char arr[] = ("abc");
+//	char att[] = { 'a', 'b', 'c','\0'};
+//	printf("%d\n", strlen(arr));
+//	printf("%d\n", strlen(att));
+//return 0;
